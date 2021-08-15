6caca9be304b668aa03d4281d17bbc2c34edd8ab
Lemon-y7 <83382571+Lemon-y7@users.noreply.github.com>
2021-08-15 21:23:49 +0800

    Create orb_feature_detection_image.py

---
+++ orb_feature_detection_image.py
@@ -0,0 +1,29 @@
+#This code is copied from https://www.youtube.com/watch?v=nnH55-zD38I&t=938s
+import cv2
+import numpy as np
+img1 = cv2.imread('pic/kitkat.jpg',0)
+img2 = cv2.imread('pic2/kit.jpg',0)
+#img2 = cv2.imread('pic/jacknjill.jpg',0)
+orb = cv2.ORB_create(nfeatures=1000)
+
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+imgKp1 = cv2.drawKeypoints(img1,kp1,None)
+imgKp2 = cv2.drawKeypoints(img2,kp2,None)
+
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des1,des2,k=2)
+good =[]
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+print(len(good))
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None, flags=2)
+
+cv2.imshow('Image Key Point',imgKp1)
+cv2.imshow('Matching',img3)
+
+
+
+cv2.waitKey(0)
